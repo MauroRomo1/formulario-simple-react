54a3d8a7725e06b871c4d8acdf8835423af388e2
Mauro Romo <mauroromo.developer@outlook.com>
2023-07-21 22:13:30 -0300

    instalo la dependencia de bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h2>Formulario simple</h2>
+      </header>
+      <main>
+        <h3>Hola mundo</h3>
+      </main>
+      <footer>
+        <h5>&copy; Todos los derechos reservados</h5>
+      </footer>
+    </>
+  );
+}
+
+export default App;
